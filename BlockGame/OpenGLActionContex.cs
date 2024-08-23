4cdb6c2e5b34f76297178a73224396adc21b66cf
Creepy_hauz <Creepy_hauz@DESKTOP-AF7VUIQ>
2024-08-23 16:52:00 +0500

    Добавьте файлы проекта.

---
+++ BlockGame/OpenGLActionContex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockGame
+{
+    public class OpenGLActionContex
+    {
+        //Очередь действий.
+        private readonly ConcurrentQueue<Action> actions = new ConcurrentQueue<Action>();
+
+
+        public void UpdateActions() //Выполнение действий из очереди.
+        {
+            while (actions.TryDequeue(out Action action))
+                action();
+        }
+
+        public void EnqueueAction(Action action) //Добавление действий в очередь.
+        {
+            actions.Enqueue(action);
+        }
+    }
+}
